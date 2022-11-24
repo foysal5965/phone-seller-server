ceb7d0c2c6fcc11f44fab69fa9359819b99bdc99
simple github <foysalahmed5965@gmail.com>
2022-11-24 18:44:18 +0600

    secific category

--- index.js
+++ index.js
@@ -14,9 +14,37 @@ app.get('/', (req,res)=>{
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.3v10x4o.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3v10x4o.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+
+async function run(){
+   
+    try{
+        const categoryCollection= client.db('full-stack-project-1').collection('Category');
+        const categoryDetailsCollection = client.db('full-stack-project-1').collection('Category-details')
+        app.get('/category', async(req,res)=>{
+            const query= {};
+            const category= await categoryCollection.find(query).toArray();
+            res.send(category)
+        })
+      app.get('/category/:id', async(req,res)=>{
+        const id= req.params.id;
+        const query= {category_id:id}
+        
+        const result= await categoryDetailsCollection.find(query).toArray()
+        console.log(result)
+        res.send(result)
+      })
+    }
+    finally{        
+
+    }
+}
+run().catch(er=>console.log(er))
 
 
 app.listen(port,()=>{})
