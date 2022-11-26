0ce542013fd436c8bc1a584bf0f05673ef77c70f
simple github <foysalahmed5965@gmail.com>
2022-11-26 12:54:31 +0600

    add product

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express= require('express');
-const cors= require('cors');
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port= process.env.PORT || 5000;
+const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 5000;
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req,res)=>{
-    res.send('full stack is going on')
+app.get('/', (req, res) => {
+  res.send('full stack is going on')
 })
 
 
@@ -18,45 +19,123 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
 
+  if (!authHeader) {
+    return res.status(401).send('unauthorized access')
+  }
+  const token = authHeader.split(' ')[1]
 
-
-async function run(){
-   
-    try{
-        const categoryCollection= client.db('full-stack-project-1').collection('Category');
-        const bookingsCollection= client.db('full-stack-project-1').collection('bookings');
-        const categoryDetailsCollection = client.db('full-stack-project-1').collection('Category-details')
-        app.get('/category', async(req,res)=>{
-            const query= {};
-            const category= await categoryCollection.find(query).toArray();
-            res.send(category)
-        })
-      app.get('/category/:id', async(req,res)=>{
-        const id= req.params.id;
-        const query= {category_id:id}
-        
-        const result= await categoryDetailsCollection.find(query).toArray()
-        console.log(result)
-        res.send(result)
-      })
-      app.post('/orders', async(req,res)=>{
-        const bookings= req.body;
-        const result= await bookingsCollection.insertOne(bookings)
-        res.send(result)
-      })
-      app.get('/orders', async(req,res)=>{
-        const email= req.query.email;
-        const query={email:email}
-        const bookings= await bookingsCollection.find(query).toArray()
-        res.send(bookings)
-      })
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbide access' })
     }
-    finally{        
-
-    }
+    req.decoded = decoded
+    next()
+  })
 }
-run().catch(er=>console.log(er))
 
 
-app.listen(port,()=>{})
+async function run() {
+
+  try {
+    const categoryCollection = client.db('full-stack-project-1').collection('Category');
+    const bookingsCollection = client.db('full-stack-project-1').collection('bookings');
+    const categoryDetailsCollection = client.db('full-stack-project-1').collection('Category-details')
+    const usersCollection = client.db('full-stack-project-1').collection('users')
+    app.get('/category', async (req, res) => {
+      const query = {};
+      const category = await categoryCollection.find(query).toArray();
+      res.send(category)
+    })
+    app.get('/category/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { category_id: id }
+
+      const result = await categoryDetailsCollection.find(query).toArray()
+
+      res.send(result)
+    })
+    app.post('/products', async(req,res)=>{
+      const product= req.body;
+      const result = await categoryDetailsCollection.insertOne(product)
+      res.send(result)
+    })
+    app.post('/orders', async (req, res) => {
+      const bookings = req.body;
+      const result = await bookingsCollection.insertOne(bookings)
+      res.send(result)
+    })
+    app.get('/orders', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+
+      const decodedEmail = req.decoded.email
+      console.log(decodedEmail, email)
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email }
+      const bookings = await bookingsCollection.find(query).toArray()
+      res.send(bookings)
+    })
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query)
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '12h' });
+        return res.send({ accessToken: token })
+      }
+      res.status(403).send({ accessToken: '' })
+    })
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users)
+    })
+    app.delete('/users/:id', async(req,res)=>{
+      const id= req.params.id;
+      const filter= {_id:ObjectId(id)};
+      const result= await usersCollection.deleteOne(filter)
+      res.send(result)
+    })
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === 'admin' })
+    })
+    app.get('/users/seller/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === 'Seller' })
+    })
+    app.get('/sellers', async(req,res)=>{
+      const query= {role:'Seller'}
+      const sellers= await usersCollection.find(query).toArray()
+      res.send(sellers)
+    })
+    app.delete('/sellers/:id', async(req,res)=>{
+      const id= req.params.id;
+      const filter= {_id:ObjectId(id)};
+      const result= await usersCollection.deleteOne(filter)
+      res.send(result)
+  })
+  }
+  finally {
+
+  }
+}
+run().catch(er => console.log(er))
+
+
+app.listen(port, () => { })
